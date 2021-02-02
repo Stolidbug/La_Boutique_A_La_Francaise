5ccfa11aed18d59ae7a5c190881b02d79b8a2c99
christophe <65019992+Stolidbug@users.noreply.github.com>
2021-02-02 14:32:15 +0100

    creation of product views and details

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,19 +2,45 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
-     * @Route("/product", name="product")
+     * @Route("/nos-produits")
      */
     public function index(): Response
     {
+        $products = $this->entityManager->getRepository(Product::class)->findAll();
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+            'products'=> $products
+        ]);
+    }
+
+
+    /**
+     * @Route("/produit/{slug}")
+     */
+    public function show($slug): Response
+    {
+        $product = $this->entityManager->getRepository(Product::class)->findOneBySlug($slug);
+
+        if (!$product){
+            return  $this->redirectToRoute('app_product_index');
+        }
+        return $this->render('product/show.html.twig', [
+            'product'=> $product
         ]);
     }
 }
